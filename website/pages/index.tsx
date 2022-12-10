a67d2199da3c21c40a3f400d9d3ef42c29d23f44
zhangchi <zhangchi@soyoung.com>
2022-12-10 23:59:05 +0800

    un

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          1Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
           {productList}
